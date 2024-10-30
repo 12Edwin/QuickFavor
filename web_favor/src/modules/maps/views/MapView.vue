fbb970818c19237011463b88de796fe2ef4c7c1e
CarlosMg96 <20203tn133@utez.edu.mx>
2024-10-30 12:19:43 -0600

    [ADD] Diseño de las targetas

--- web_favor/src/modules/maps/views/MapView.vue
+++ web_favor/src/modules/maps/views/MapView.vue
@@ -1,15 +1,128 @@
 <template>
+  <div class="back-container">
+    <WaveComponent />
+  </div>
+  <div class="container">
+    <v-row justify="center">
+      <v-col class="md-8">
+        <v-card class="card-custom">
+          <div class="card-header d-flex align-center justify-space-between">
+            <h2 class="header-title">
+              <v-icon icon="fa:fas fa-circle-notch fa-spin"></v-icon> ¿Dónde Estoy?
+            </h2>
+            <div class="switch-container">
+              <label class="switch">
+                <input type="checkbox">
+                <span class="slider"></span>
+              </label>
+            </div>
+          </div>
+          <!-- content -->
+          <div>
 
+          </div>
+        </v-card>
+      </v-col>
+    </v-row>
+  </div>
 </template>
 
 <script lang="ts">
-import {defineComponent} from 'vue'
+import { defineComponent } from 'vue'
+import WaveComponent from '@/components/WaveComponent.vue';
 
 export default defineComponent({
-  name: "MapView"
+  name: "MapView",
+  components: { WaveComponent }
 })
 </script>
 
 <style scoped>
+.back-container {
+  width: 100%;
+  overflow: hidden;
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  z-index: -1;
+}
+.card-custom {
+  position: relative; 
+  margin-left: 16px;
+  margin-right: 16px;
+  border-top-left-radius: 16px;
+  border-top-right-radius: 16px;
+  border-bottom-left-radius: 0;
+  border-bottom-right-radius: 0;
+  height: 88vh;
+  background-color: rgba(255, 255, 255, 0.5); 
+  backdrop-filter: blur(10px);
+  overflow: visible; 
+  z-index: 1; 
+}
+.card-header {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0 16px;
+  background-color: #566981;
+  border-top-left-radius: 16px;
+  border-top-right-radius: 16px;
+  height: 64px;
+}
+.header-title {
+  color: white;
+  font-size: 1.25rem;
+}
+.switch-container {
+  display: flex;
+  align-items: center;
+}
+
+.switch {
+  position: relative;
+  display: inline-block;
+  width: 48px;
+  height: 24px;
+}
+
+.switch input {
+  opacity: 0;
+  width: 0;
+  height: 0;
+}
+
+.slider {
+  position: absolute;
+  cursor: pointer;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: #CB5E5E;
+  transition: 0.4s;
+  border-radius: 24px; /* Bordes redondeados */
+}
+
+.slider:before {
+  position: absolute;
+  content: "";
+  height: 18px;
+  width: 18px;
+  left: 4px;
+  bottom: 3px;
+  background-color: white;
+  transition: 0.4s;
+  border-radius: 50%;
+}
+
+/* Estado activado */
+input:checked + .slider {
+  background-color: #4caf50;
+}
+
+input:checked + .slider:before {
+  transform: translateX(24px);
+}
 
 </style>
