b934157bf5e7e30cf10055a38e77e42b36bffea9
Edwin <20213tn046@utez.edu.mx>
2024-11-11 13:57:45 -0600

    [FIX] favor status page for web version

--- web_favor/src/layouts/PrivateLayout.vue
+++ web_favor/src/layouts/PrivateLayout.vue
@@ -3,8 +3,8 @@
     <div class="d-flex flex-column flex-md-row ">
       <SideBar :class="{ 'active': isSidebarOpen }"/>
       <div class="content">
-        <div class="header">
-          <span class="text-light text me-3">Juan Rodrígo</span>
+        <div class="header-layout">
+          <span class="text-light text mr-3">Juan Rodrígo</span>
         </div>
         <div class="content-body">
           <router-view></router-view>
@@ -33,12 +33,12 @@ export default defineComponent({
 
 .content {
   flex-grow: 1;
-  overflow: hidden;
+  overflow-y: auto;
+  overflow-x: hidden;
   height: 100vh;
   position: relative;
   -ms-overflow-style: none;
   scrollbar-width: none;
-  height: 100vh;
   display: flex;
   flex-direction: column;
 }
@@ -52,22 +52,24 @@ body::-webkit-scrollbar, .content::-webkit-scrollbar {
 /* Cuerpo del contenido con padding */
 .content-body {
   padding-top: 16px;
+  position: relative;
 }
 
 /* Encabezado sticky en la parte superior */
-.header {
+.header-layout {
   z-index: 1;
   position: sticky;
   top: 0;
   left: 0;
   width: 100%;
-  height: 8vh;
+  padding-top: calc(4dvh - 13px);
+  padding-bottom: calc(4dvh - 13px);
   background-color: rgba(137, 167, 177, 0.6);
   backdrop-filter: blur(5px);
   display: flex;
   justify-content: flex-end;
   align-items: center;
-  padding-right: 16px; /* Espacio extra a la derecha */
+  padding-right: 16px;
 }
 
 /* Estilos específicos para pantallas pequeñas */
