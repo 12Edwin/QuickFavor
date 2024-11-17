af9dded74787435fcf0a96c427f7af89c4a20ebe
CarlosMg96 <rguezcarlosmg96@outlook.com>
2024-11-16 21:05:02 -0600

    [UPDATE]

--- backend/src/commons/connection-db.ts
+++ backend/src/commons/connection-db.ts
@@ -5,7 +5,7 @@ dotenv.config();
 
 const pool = mysql.createPool({
     host: process.env.DB_HOST,
-    user: process.env.DB_USERNAME,
+    user: 'root',
     password: process.env.DB_ROOT_PASSWORD,
     database: process.env.DB_NAME,
     port: 3306,
